199c5282289318a21f0a187f8e3ae71b9efeec9e
training <marcin.danilewicz@mobica.com>
2018-07-19 16:04:54 +0200

    4_51 input accesory and IBDesignable

--- window-shopper/window-shopper/Controller/MainVC.swift
+++ window-shopper/window-shopper/Controller/MainVC.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageTxt: CurrencyTxtFIeld!
+    @IBOutlet weak var priceTxt: CurrencyTxtFIeld!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60
+        ))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.setTitle("CALCULATE", for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        // for: when button is pressed
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+        
     }
 
+    @objc func calculate() {
+        print("we got here")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- window-shopper/window-shopper/VIews/CurrencyTxtFIeld.swift
+++ window-shopper/window-shopper/VIews/CurrencyTxtFIeld.swift
@@ -8,8 +8,26 @@
 
 import UIKit
 
+@IBDesignable
+
 class CurrencyTxtFIeld: UITextField {
 
+    func customizeView () {
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2462007705)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0) //white ( with color Literal)
+        }
+    }
+
+    override func prepareForInterfaceBuilder() {
+        customizeView ()
+    }
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -19,16 +37,6 @@ class CurrencyTxtFIeld: UITextField {
     */
     override func awakeFromNib() {
         super.awakeFromNib()
-        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2462007705)
-        layer.cornerRadius = 5.0
-        textAlignment = .center
-
-        
-        if let p = placeholder {
-            let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
-            attributedPlaceholder = place
-            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0) //white ( with color Literal)
-        }
-        
+        customizeView ()
     }
 }
